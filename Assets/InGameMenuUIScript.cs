caacc2388b61ff7faff084e859d07d8a793d0422
Mjones090 <145160703+Mjones090@users.noreply.github.com>
2023-10-23 23:01:43 +0100

    In Game Menu

---
+++ Assets/InGameMenuUIScript.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class InGameMenuUIScript : MonoBehaviour
+{
+    public GameObject menuCanvas;
+
+    public void OnButtonPress(string buttonText)
+
+    { 
+    switch (buttonText)
+        {
+            case "MainMenu":
+                {
+                    SceneManager.LoadScene("MenuScene");
+                    break;
+                }
+
+            case "Back":
+                {
+                    menuCanvas.SetActive(false);
+                    break;
+                }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P)) 
+        {
+            menuCanvas.SetActive(!menuCanvas.activeSelf);
+        }
+    }
+}
